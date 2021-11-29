3bca9abd5b33a92559d37d9b2b6b99c65121866b
Ltmodz95 <mohamed.a.hussein.elsayed@gmail.com>
2021-11-29 22:09:11 +0200

    adding the recipe page

--- src/Components/recipe/Recipe.tsx
+++ src/Components/recipe/Recipe.tsx
@@ -1,12 +1,53 @@
 import React from 'react'
 import { useParams } from 'react-router';
+import clock from '../../assets/images/clock.png';
+import serving from '../../assets/images/serving.png';
+import comment from '../../assets/images/comment.png';
+import './Recipe.scss';
+
 export default function Recipe() {
 
     const { id } = useParams();
 
     return (
-        <div>
-            a7a
+        <div className="recipe-container">
+            <div className="basic-info">
+                <h1>Agneau à l'abricot (recette turque)</h1>
+                <div className="data">
+                    <div><img src={clock}/> &nbsp;5 min</div>
+                    <div><img src={serving}/> &nbsp;3 serv.</div>
+                    <div>Easy</div>
+                    <div><img src={comment}/> &nbsp;5</div>
+                </div>
+            </div>
+            <div className="more-info">
+                <div className="ingridents-steps">
+                    <div>
+                        <h3>Ingridents:</h3>
+                        <ul>
+                            <li>a</li>
+                            <li>b</li>
+                            <li>c</li>
+                            <li>d</li>
+                            <li>a</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3>Steps:</h3>
+                        <ul>
+                            <li>a</li>
+                            <li>b</li>
+                            <li>c</li>
+                            <li>d</li>
+                            <li>a</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="recipe-image">
+                    <img src="https://assets.afcdn.com/recipe/20150901/17089_w420h344c1cx1500cy2250.jpg"/>
+                </div>
+            </div>
         </div>
     )
 }
